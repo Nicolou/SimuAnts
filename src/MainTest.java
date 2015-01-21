a06d2315506d6f52c141dcd3be67a27d5a04dc6c
nicolou <nicolas.montarnal@free.fr>
2015-01-21 14:46:15 +0100

    main parameters used to dertermine the numbers of insects to show.

--- src/MainTest.java
+++ src/MainTest.java
@@ -8,17 +8,26 @@ public class MainTest {
 		model.SpaceField sp = new model.SpaceField();
 		sp.setDimX(800);sp.setDimY(600);
 		
-		for (int i=0; i<40; i++) {
+		int max1=40, max2=5;
+		try {
+			if (args.length>0) max1 = Integer.parseInt(args[0]);
+			if (args.length>1) max2 =Integer.parseInt(args[1]);
+		}
+		catch (NumberFormatException  ex) {
+			System.err.println("usage: parameter integer...");
+		}
+		
+		for (int i=0; i<max1; i++) {
 			model.Ant ant1 = new model.Ant(sp);
 			ant1.setName("fourmis_" + i);
-			ant1.setPosx(100);ant1.setPosy(100);
+			ant1.setPosx(sp.getDimX()/2);ant1.setPosy(sp.getDimY()/2);
 			ant1.simu();			
 		}
 
-		for (int i=0; i<5 ; i++) {
+		for (int i=0; i<max2 ; i++) {
 			model.OldAnt ant2 = new model.OldAnt(sp);
 			ant2.setName("toto");
-			ant2.setPosx(50);ant2.setPosy(50);
+			ant2.setPosx(sp.getDimX()/2);ant2.setPosy(sp.getDimY()/2);
 			ant2.simu();
 		}
 		
